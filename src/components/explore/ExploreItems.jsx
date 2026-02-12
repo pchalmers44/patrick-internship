88d12e16a34194c882f0dcf6f414c50d47fe3461
pchalmers44 <pchalmers44@gmail.com>
2026-02-11 21:13:01 -0500

    Fixed anchor tags and unused variables for vercel build

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -176,13 +176,13 @@ const ExploreItems = () => {
                         <button>Buy Now</button>
                         <div className="nft__item_share">
                           <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer">
+                          <a href="#">
                             <i className="fa fa-facebook fa-lg"></i>
                           </a>
-                          <a href="" target="_blank" rel="noreferrer">
+                          <a href="#">
                             <i className="fa fa-twitter fa-lg"></i>
                           </a>
-                          <a href="">
+                          <a href="#">
                             <i className="fa fa-envelope fa-lg"></i>
                           </a>
                         </div>
